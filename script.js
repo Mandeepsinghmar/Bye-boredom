f00b446f8b336d0d167f7e3f907c69a5baa6a480
Mandeep <singhmar1655@gmail.com>
2020-12-09 21:30:43 +0530

    bye-boredom

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch('https://www.boredapi.com/api/activity/')
+  .then(response => response.json())
+  .then(data => {
+     document.getElementById("activity").innerHTML = data.activity
+     document.getElementById("type").innerHTML = data.type
+     document.getElementById("participants").innerHTML = data.participants
+    } );
