d4c0540051a282c25c07902b5f3edfb3748c0c60
Mandeep <singhmar1655@gmail.com>
2020-12-10 11:10:15 +0530

    docs: update in document
    Removed the image from main page.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 fetch('https://www.boredapi.com/api/activity/')
   .then(response => response.json())
   .then(data => {
+ 
      document.getElementById("activity").innerHTML = data.activity
      document.getElementById("type").innerHTML = data.type
      document.getElementById("participants").innerHTML = data.participants
+  
     } );
